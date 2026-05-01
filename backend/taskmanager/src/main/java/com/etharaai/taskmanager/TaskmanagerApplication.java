63c68ed755790166bac04c865e0d9cb0158725a5
yusuf7861 <yjamal710@gmail.com>
2026-05-02 03:17:42 +0530

    feat: configure application for WAR packaging and enable servlet support

--- backend/taskmanager/src/main/java/com/etharaai/taskmanager/TaskmanagerApplication.java
+++ backend/taskmanager/src/main/java/com/etharaai/taskmanager/TaskmanagerApplication.java
@@ -2,9 +2,16 @@ package com.etharaai.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskmanagerApplication {
+public class TaskmanagerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskmanagerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
